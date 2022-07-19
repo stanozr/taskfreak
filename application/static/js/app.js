26d1a46e220509d6e77a71cd206404175c6db848
Stan Ozier <stanislaso@spc.int>
2022-07-19 17:57:50 +1100

    Task view modal + js improvements

--- application/static/js/app.js
+++ application/static/js/app.js
@@ -1,10 +1,42 @@
 $('document').ready(function(){
-  $('.hamburger').click(function() {
-    $('.sidebar').toggleClass('show')
-  });
+
+  var taskView = $('#viewModal');
+  if (taskView[0]) {
+    taskView.on('hidden.bs.modal', function() {
+      $(this).find('.modal-header').removeClass('bg-danger bg-warning bg-info bg-light text-light');
+    });
+    $('.task-title').click(function() {
+      $t = $(this);
+      prcl = '';
+      prio = $t.closest('.task-row').find('.task-deadline');
+      if (prio.hasClass('priority-3')) {
+        prcl = 'bg-danger text-light';
+      } else if (prio.hasClass('priority-2')) {
+        prcl = 'bg-warning';
+      } else if (prio.hasClass('priority-1')) {
+        prcl = 'bg-info';
+      } else {
+        prcl = 'bg-light';
+      }
+      $('#viewModal .modal-header').addClass(prcl).find('h5').text($t.text());
+      new bootstrap.Modal('#viewModal').show();
+    });
+  }
+
+  $fss = $('#original-filter-status');
+  if ($fss[0]) {
+    $fss.clone().prop('id', 'copy-filter-status').appendTo('#search-filter-status');
+  }
+
+  $fus = $('#original-filter-users');
+  if ($fus[0]) {
+    $fus.clone().prop('id', 'copy-filter-users').removeClass('list-group-flush').appendTo('#search-filter-users');
+  }
+
   $('.btn-status label').click(function() {
     $(this).toggleClass('btn-active');
   });
+
   $('.btn-users label').click(function() {
     $t = $(this);
     if (!$t.hasClass('btn-active')) {
@@ -12,11 +44,17 @@ $('document').ready(function(){
       $t.toggleClass('btn-active');
     }
   });
+
+  $('.hamburger').click(function() {
+    $('.sidebar').toggleClass('show')
+  });
+
   $('.dropdown-item').click(function(e) {
     if (e.target === e.currentTarget) {
       $(this).children('input').click();
     }
   });
+
   $(".dropdown input[type='checkbox']").change(function() {
     $p = $(this).closest('.dropdown');
     $c = $p.find("input[type='checkbox']");
@@ -28,5 +66,6 @@ $('document').ready(function(){
     });
     $p.find('.dropdown-count').text(t);
   })
+
   $('.datepicker').datepicker();
 });
